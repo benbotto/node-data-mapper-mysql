91bee43c7d4d03e4497a5786fe9eb3a405e543ae
benbotto <benbotto@users.noreply.github.com>
2016-12-04 13:46:51 -0800

    Bootstrap now uses scriptGarner.

--- bootstrap.js
+++ bootstrap.js
@@ -4,36 +4,17 @@
  * This script sets up the dependency injection container, insulin.
  */
 const insulin = require('insulin');
+const scripts = (require('./grunt/scriptGarner.js'))().app;
 
 // Static dependencies.
-insulin
-  .factory('mysql', () => require('mysql'));
+insulin.factory('mysql', () => require('mysql'))
 
-// Registers itself with insulin under the ndm namespace.
+// Node-data-mapper registers itself with insulin under the ndm namespace.
 require('node-data-mapper');
-require('ndm-schema-generator-mysql');
 
 // Application (dynamic) dependencies.
-const glob = require('glob');
-const opts = {
-  cwd: __dirname,
-  ignore: [
-    './node_modules/**',
-    './example/**',
-    './doc/**',
-    './grunt/**',
-    './Gruntfile.js',
-    './**/*Spec.js',
-    './bootstrap.js',
-    './index.js'
-  ]
-};
-
-const files = glob.sync('./**/*.js', opts);
-
-// Let each file register itself with the DiC.
-files.forEach(require);
+scripts.forEach(script => require(script));
 
 // Export the list of files.
-module.exports = files;
+module.exports = scripts;
 
