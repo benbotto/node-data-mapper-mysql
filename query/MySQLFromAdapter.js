1931c1d6a3b387ed590d403a6847a38a1170199a
benbotto <benbotto@users.noreply.github.com>
2016-12-30 15:11:21 -0800

    MySQLFromAdapter started. Works with Selects.

---
+++ query/MySQLFromAdapter.js
@@ -0,0 +1,51 @@
+'use strict';
+
+require('insulin').factory('ndm_MySQLFromAdapter',
+  ['ndm_FromAdapter', 'ndm_MySQLSelect', 'ndm_Update', 'ndm_Delete'],
+  ndm_MySQLFromAdapterProducer);
+
+function ndm_MySQLFromAdapterProducer(FromAdapter, MySQLSelect, Update, Delete) {
+  /**
+   * An specialization of the FromAdapter class for MySQL.
+   * @extends FromAdapter
+   */
+  class MySQLFromAdapter extends FromAdapter {
+    /**
+     * Select from the table.
+     * @see Select#select
+     * @param {...(object|string)=} cols - An optional set of columns to select.
+     * @return {MySQLSelect} A MySQLSelect instance that can be executed.
+     */
+    select(...cols) {
+      const sel = new MySQLSelect(this, this._queryExecuter);
+
+      // This has to be applied because cols is optional.  If cols is not passed,
+      // calling sel.select(cols) would pass undefined to select().
+      return sel.select.apply(sel, cols);
+    }
+
+    /**
+     * Delete from the table.
+     * @param {string} tableAlias - The unique alias of the table from which
+     * records will be deleted.  Optional, defaults to the alias of the from
+     * table.
+     * @return {Delete} A Delete instance that can be executed.
+     */
+    delete(tableAlias) {
+      return new Delete(this, tableAlias);
+    }
+
+    /**
+     * Update a table.
+     * @param {Object} model - The model describing what to update.
+     * @see Update
+     * @return {Update} An Update instance that can be executed.
+     */
+    update(model) {
+      return new Update(this, model);
+    }
+  }
+
+  return MySQLFromAdapter;
+}
+
---
+++ query/MySQLFromAdapterSpec.js
@@ -0,0 +1,113 @@
+describe('MySQLFromAdapter()', function() {
+  'use strict';
+
+  const insulin          = require('insulin');
+  const MySQLFromAdapter = insulin.get('ndm_MySQLFromAdapter');
+  const MySQLEscaper     = insulin.get('ndm_MySQLEscaper');
+  const db               = insulin.get('ndm_testDB');
+  const escaper          = new MySQLEscaper();
+  let   qryExec;
+
+  beforeEach(() => qryExec = jasmine.createSpyObj('qryExec', ['select', 'delete']));
+
+  /**
+   * Ctor.
+   */
+  describe('.constructor()', function() {
+    it('extends FromAdapter.', function() {
+      const FromAdapter = insulin.get('ndm_FromAdapter');
+      const fa          = new MySQLFromAdapter(db, escaper, qryExec, 'users u');
+
+      expect(fa instanceof FromAdapter).toBe(true);
+      expect(fa.database).toBe(db);
+      expect(fa.queryExecuter).toBe(qryExec);
+      expect(fa.escaper).toBe(escaper);
+    });
+  });
+
+  /**
+   * Select.
+   */
+  describe('.select().', function() {
+    it('returns a MySQLSelect instance.', function() {
+      const MySQLSelect = insulin.get('ndm_MySQLSelect');
+      const sel         = new MySQLFromAdapter(db, escaper, qryExec, {table: 'users'})
+        .select();
+
+      expect(sel instanceof MySQLSelect).toBe(true);
+    });
+
+    it('selects all columns by default.', function() {
+      const sel = new MySQLFromAdapter(db, escaper, qryExec, {table: 'users'})
+        .select();
+
+      expect(sel.toString()).toBe(
+        'SELECT  `users`.`userID` AS `users.userID`,\n' +
+        '        `users`.`firstName` AS `users.firstName`,\n' +
+        '        `users`.`lastName` AS `users.lastName`\n' +
+        'FROM    `users` AS `users`');
+    });
+
+    it('can be passed columns explicitly.', function() {
+      const sel = new MySQLFromAdapter(db, escaper, qryExec, {table: 'users'})
+        .select('users.userID', 'users.firstName');
+
+      expect(sel.toString()).toBe(
+        'SELECT  `users`.`userID` AS `users.userID`,\n' +
+        '        `users`.`firstName` AS `users.firstName`\n' +
+        'FROM    `users` AS `users`');
+    });
+  });
+
+  /**
+   * Delete.
+   */
+  xdescribe('.delete()', function() {
+    it('returns a Delete instance.', function() {
+      const Delete = insulin.get('ndm_Delete');
+      const del    = new MySQLFromAdapter(db, escaper, qryExec, 'users')
+        .where({$eq: {'users.userID': 1}})
+        .delete();
+
+      expect(del instanceof Delete).toBe(true);
+    });
+
+    it('can be provided an optional table alias.', function() {
+      const del    = new MySQLFromAdapter(db, escaper, qryExec, 'users u')
+        .where({$eq: {'u.userID': 1}})
+        .delete();
+
+      expect(del.toString()).toBe(
+        'DELETE  `u`\n' +
+        'FROM    `users` AS `u`\n' +
+        'WHERE   `u`.`userID` = 1');
+    });
+  });
+
+  /**
+   * Update.
+   */
+  xdescribe('.update()', function() {
+    it('extends Update.', function() {
+      const Update = insulin.get('ndm_Update');
+      const upd    = new MySQLFromAdapter(db, escaper, qryExec, 'users u')
+        .where({$eq: {'u.userID': 1}})
+        .update({'u.firstName': 'Joe'});
+
+      expect(upd instanceof Update).toBe(true);
+    });
+
+    it('passes the model to the Update constructor.', function() {
+      const upd = new MySQLFromAdapter(db, escaper, qryExec, 'users u')
+        .where({$eq: {'u.userID': 1}})
+        .update({'u.firstName': 'Joe'});
+
+      expect(upd.toString()).toBe(
+        'UPDATE  `users` AS `u`\n' +
+        'SET\n' +
+        '`u`.`firstName` = :u_firstName_0\n' +
+        'WHERE   `u`.`userID` = 1');
+    });
+  });
+});
+
